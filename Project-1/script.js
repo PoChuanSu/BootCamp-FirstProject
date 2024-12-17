3c33aa75e05ca04d5e5ec7c54e80ba365d1a7c5f
PoChuanSu <peterpeter1010@live.com>
2024-12-17 18:16:58 +1100

    arrow function

--- Project-1/script.js
+++ Project-1/script.js
@@ -38,30 +38,38 @@ const age1 = calcFactAge(2015);
 console.log(age1);
 console.log(calcFactAge(2037));
 
-if (votesInteresting === votesMindblowing) {
-    alert("This is the first if / else statement");
-} else if (votesInteresting > votesMindblowing) {
-    console.log("Interesting fact!");
-} else if (votesInteresting < votesMindblowing) {
-    console.log("Mindblowing fact!");
-}
+const calcFactAge2 = (year) =>
+    year <= new Date().getFullYear()
+        ? new Date().getFullYear() - year
+        : `Impossible year. Year needs to be less or equal ${new Date().getFullYear()}`;
 
-//falsy value: 0, '', null, undefined
-// truthy value: everything else;
-if (votesMindblowing) {
-    console.log("Mindblowing fact!");
-} else {
-    console.log("Not so special...");
-}
+console.log(calcFactAge2(2015));
+console.log(calcFactAge2(2037));
 
-const text = "Lisbon is the capital of Portugal";
-const upperText = text.toUpperCase();
-console.log(upperText);
+// if (votesInteresting === votesMindblowing) {
+//     alert("This is the first if / else statement");
+// } else if (votesInteresting > votesMindblowing) {
+//     console.log("Interesting fact!");
+// } else if (votesInteresting < votesMindblowing) {
+//     console.log("Mindblowing fact!");
+// }
 
-const str = `The current fact is ${text}. It is ${calcFactAge(
-    2015
-)} years old. It is probably ${
-    totalUpvotes > votesFalse ? "correct" : "not ture"
-}`;
+// //falsy value: 0, '', null, undefined
+// // truthy value: everything else;
+// if (votesMindblowing) {
+//     console.log("Mindblowing fact!");
+// } else {
+//     console.log("Not so special...");
+// }
 
-console.log(str);
+// const text = "Lisbon is the capital of Portugal";
+// const upperText = text.toUpperCase();
+// console.log(upperText);
+
+// const str = `The current fact is ${text}. It is ${calcFactAge(
+//     2015
+// )} years old. It is probably ${
+//     totalUpvotes > votesFalse ? "correct" : "not ture"
+// }`;
+
+// console.log(str);
