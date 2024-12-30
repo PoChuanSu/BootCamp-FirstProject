683ac0dfc4ac789812a1997e1a0fe37de72b9a65
PoChuanSu <peterpeter1010@live.com>
2024-12-30 12:59:30 +1100

    interesting button votes

--- Project-1/src/App.js
+++ Project-1/src/App.js
@@ -44,7 +44,11 @@ function App() {
 
             <main className="main">
                 <CategoryFilter setCurrentCategory={setCurrentCategory} />
-                {isLoading ? <Loader /> : <FactList facts={facts} />}
+                {isLoading ? (
+                    <Loader />
+                ) : (
+                    <FactList facts={facts} setFacts={setFacts} />
+                )}
             </main>
         </>
     );
@@ -215,7 +219,7 @@ function CategoryFilter({ setCurrentCategory }) {
     );
 }
 
-function FactList({ facts }) {
+function FactList({ facts, setFacts }) {
     if (facts.length === 0)
         return (
             <p className="message">
@@ -227,7 +231,7 @@ function FactList({ facts }) {
         <section>
             <ul className="facts-list">
                 {facts.map((fact) => (
-                    <Fact key={fact.id} fact={fact} />
+                    <Fact key={fact.id} fact={fact} setFacts={setFacts} />
                 ))}
             </ul>
             <p>There are {facts.length} facts in the database. Add your own!</p>
@@ -236,10 +240,24 @@ function FactList({ facts }) {
 }
 
 // function Fact (props) {
-function Fact({ fact }) {
-    // console.log(props);
-    // const { factObj } = props;
-    // const factObj = props.factObj;
+function Fact({ fact, setFacts }) {
+    const [isUpdating, setIsUpdating] = useState(false);
+
+    async function handleVote() {
+        setIsUpdating(true);
+        const { data: updatedFact, error } = await supabase
+            .from("facts")
+            .update({ votesInteresting: fact.votesInteresting + 1 })
+            .eq("id", fact.id)
+            .select();
+        setIsUpdating(false);
+
+        console.log(updatedFact);
+        if (!error)
+            setFacts((facts) =>
+                facts.map((f) => (f.id === fact.id ? updatedFact[0] : f))
+            );
+    }
 
     return (
         <li className="fact">
@@ -265,7 +283,9 @@ function Fact({ fact }) {
                 {fact.category}
             </span>
             <div className="vote-buttons">
-                <button>👍 {fact.votesInteresting}</button>
+                <button onClick={handleVote} disabled={isUpdating}>
+                    👍 {fact.votesInteresting}
+                </button>
                 <button>🤯 {fact.votesMindblowing}</button>
                 <button>⛔️ {fact.votesFalse}</button>
             </div>
