1f424cf002ea8a7839b09141898823e49b4af27b
PoChuanSu <peterpeter1010@live.com>
2024-12-30 13:04:18 +1100

    all votes buttons function

--- Project-1/src/App.js
+++ Project-1/src/App.js
@@ -243,11 +243,11 @@ function FactList({ facts, setFacts }) {
 function Fact({ fact, setFacts }) {
     const [isUpdating, setIsUpdating] = useState(false);
 
-    async function handleVote() {
+    async function handleVote(columnName) {
         setIsUpdating(true);
         const { data: updatedFact, error } = await supabase
             .from("facts")
-            .update({ votesInteresting: fact.votesInteresting + 1 })
+            .update({ [columnName]: fact[columnName] + 1 })
             .eq("id", fact.id)
             .select();
         setIsUpdating(false);
@@ -283,11 +283,24 @@ function Fact({ fact, setFacts }) {
                 {fact.category}
             </span>
             <div className="vote-buttons">
-                <button onClick={handleVote} disabled={isUpdating}>
+                <button
+                    onClick={() => handleVote("votesInteresting")}
+                    disabled={isUpdating}
+                >
                     👍 {fact.votesInteresting}
                 </button>
-                <button>🤯 {fact.votesMindblowing}</button>
-                <button>⛔️ {fact.votesFalse}</button>
+                <button
+                    onClick={() => handleVote("votesMindblowing")}
+                    disabled={isUpdating}
+                >
+                    🤯 {fact.votesMindblowing}
+                </button>
+                <button
+                    onClick={() => handleVote("votesFalse")}
+                    disabled={isUpdating}
+                >
+                    ⛔️ {fact.votesFalse}
+                </button>
             </div>
         </li>
     );
